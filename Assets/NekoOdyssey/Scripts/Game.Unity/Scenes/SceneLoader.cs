cc2d2f6d74df9fdaf3cdc9c6870b10892bb0242c
jirakorn <dotpng@gmail.com>
2024-01-06 21:40:25 +0700

    ii

--- Assets/NekoOdyssey/Scripts/Game.Unity/Scenes/SceneLoader.cs
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Scenes/SceneLoader.cs
@@ -11,6 +11,8 @@ namespace NekoOdyssey.Scripts.Game.Unity.Scenes
             if (!Application.isEditor)
             {
                 SceneManager.LoadScene($"Neko2", LoadSceneMode.Single);
+                SceneManager.LoadScene($"Neko08", LoadSceneMode.Additive);
+                SceneManager.LoadScene($"CatScene", LoadSceneMode.Additive);
                 SceneManager.LoadScene($"SkyBox", LoadSceneMode.Additive);
                 SceneManager.LoadScene($"NekoRoad", LoadSceneMode.Additive);
                 SceneManager.LoadScene($"GameMain", LoadSceneMode.Additive);
