fb18e944453b6e47f2edd509e98155ae92a357c7
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-02-05 10:19:16 +0700

    fix: change methods to load asset packed bundles

--- Assets/NekoOdyssey/Scripts/Game.Unity/AssetBundles/AssetBundleLoader.cs
+++ Assets/NekoOdyssey/Scripts/Game.Unity/AssetBundles/AssetBundleLoader.cs
@@ -14,53 +14,28 @@ namespace NekoOdyssey.Scripts.Game.Unity.AssetBundles
 
         private void Awake()
         {
-            var bundleNames = new List<string>();
-            bundleNames.Add($"dialogcanvas");
-            bundleNames.Add($"a01sleepsnap");
-            bundleNames.Add($"a01snap");
-            bundleNames.Add($"a02snap");
-            bundleNames.Add($"a04snap");
-            bundleNames.Add($"a06snap");
-            bundleNames.Add($"a07snap");
-            bundleNames.Add($"a11snap");
-            bundleNames.Add($"a12snap");
-            bundleNames.Add($"a14snap");
-            bundleNames.Add($"b01snap");
-            bundleNames.Add($"b08snap");
-            bundleNames.Add($"b09snap");
-            bundleNames.Add($"b12boxsnap");
-            bundleNames.Add($"b12snap");
-            bundleNames.Add($"b13snap");
-            bundleNames.Add($"b14snap");
-            bundleNames.Add($"c04snap");
-            bundleNames.Add($"c05snap");
-            bundleNames.Add($"c10snap");
-            bundleNames.Add("cat_snaps");
-
-            foreach (var action in Enum.GetValues(typeof(PlayerMenuAction)))
+            var bundleNames = new List<string>
             {
-                if ((PlayerMenuAction)action == PlayerMenuAction.None) continue;
-                var actionName = Enum.GetName(typeof(PlayerMenuAction), action);
-                var bundleName = $"{actionName.ToLower()}action";
-                bundleNames.Add(bundleName);
-            }
+                $"dialogcanvas",
+                $"menu_actions",
+                $"cat_snaps"
+            };
 
-            foreach (var name in bundleNames)
+            foreach (var bundleName in bundleNames)
             {
-                LoadAssetBundle(name);
+                LoadAssetBundle(bundleName);
             }
             
             GameRunner.Instance.SetReady(true);
         }
         
-        private void LoadAssetBundle(string name)
+        private void LoadAssetBundle(string bundleName)
         {
             var bundlePath = System.IO.Path.Combine(
                 Application.streamingAssetsPath,
                 "SwitchAssetBundles",
-                name
+                bundleName
             );
-            Debug.Log($">>load<< 00 {name} {System.IO.File.Exists(bundlePath)}");
             if (!System.IO.File.Exists(bundlePath)) return;
 
             var request = AssetBundle.LoadFromFile(bundlePath);
