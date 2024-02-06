7e71c3812d4839e735d01d44d4ebb52cdf04ad2a
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-02-06 12:21:30 +0700

    chore: add ai core logic

---
+++ Assets/NekoOdyssey/Scripts/Game.Core/Ais/GameAis.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using NekoOdyssey.Scripts.Game.Core.Ais.Cat;
+
+namespace NekoOdyssey.Scripts.Game.Core.Ais
+{
+    public class GameAis
+    {
+        public CatAi CatAi { get; } = new();
+
+        public void Bind()
+        {
+        }
+
+        public void Start()
+        {
+        }
+
+        public void Unbind()
+        {
+        }
+        
+    }
+}
