e5a6f012bae70a4c4b2a7324eaa51559ae602017
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-04-03 20:18:12 +0700

    fix: fix call to feed bugs

--- Assets/NekoOdyssey/Scripts/Game.Unity/Ais/Cat/Behaviours/Move/MoveController.cs
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Ais/Cat/Behaviours/Move/MoveController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
+using DG.Tweening;
 using NekoOdyssey.Scripts.Game.Core.Ais.Cat;
+using NekoOdyssey.Scripts.Game.Core.Ais.Cat.Behaviours;
 using UniRx;
 using UnityEngine;
 
@@ -12,29 +14,63 @@ namespace NekoOdyssey.Scripts.Game.Unity.Ais.Cat.Behaviours.Move
         private CatAi _catAi;
         private CatBehaviourController _behaviourController;
         private Animator _animator;
+        private SpriteRenderer _renderer;
+        private bool _running;
+        private bool _moving;
+        private bool _flipped;
+        private Coroutine _currentCoroutine;
 
         private void Awake()
         {
             var behaviourController = GetComponent<CatBehaviourController>();
             _catAi = behaviourController.CatAi;
+            _renderer = GetComponent<SpriteRenderer>();
             _animator = GetComponent<Animator>();
         }
 
         private void Start()
         {
+            _catAi.OnFlip
+                .Subscribe(HandleFlip)
+                .AddTo(this);
+            _catAi.OnChangeMode
+                .Subscribe(HandleModeChange)
+                .AddTo(this);
             _catAi.OnCatMove
-                .Subscribe(HandleFollowPlayer)
+                .Subscribe(HandleMove)
                 .AddTo(this);
         }
 
-        private void HandleFollowPlayer(Vector3 position)
+        private void HandleFlip(bool flipped)
         {
-            StartCoroutine(MoveToPosition(position));
+            _flipped = flipped;
+        }
+
+        private void HandleModeChange(CatBehaviourMode mode)
+        {
+            _running = _catAi.Mode == CatBehaviourMode.FollowPlayer;
+            if (!_running && _currentCoroutine != null) StopCoroutine(_currentCoroutine);
+            if (_running) _catAi.WaitingToWalk = true;
+            if (!_running) _animator.SetBool($"Move", false);
+        }
+
+        private void HandleMove(Vector3 position)
+        {
             _animator.SetBool($"Move", true);
+            _currentCoroutine = StartCoroutine(MoveToPosition(position));
         }
 
         private IEnumerator MoveToPosition(Vector3 position)
         {
+            if (!_running) yield break;
+            if (_catAi.WaitingToWalk)
+            {
+                yield return null;
+                _catAi.SetCatPosition(transform.position);
+                yield break;
+            }
+
+            _renderer.flipX = _flipped;
             transform.Translate(position, Space.World);
             yield return null;
             _catAi.SetCatPosition(transform.position);
---
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Cats/CatStateMachineBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace NekoOdyssey.Scripts.Game.Unity.Cats
+{
+    public class CatStateMachineBehavior : StateMachineBehaviour
+    {
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.shortNameHash == Animator.StringToHash($"Walk"))
+            {
+                Debug.Log($">>walk_ready<< {animator.transform.gameObject}");
+                var gameObject = animator.transform.gameObject;
+                var eligibleAis = GameRunner.Instance.Core.Ais.CatAis.Where(catAi => catAi.GameObject = gameObject);
+                foreach (var ai in eligibleAis) ai.WaitingToWalk = false;
+            }
+        }
+    }
+}
