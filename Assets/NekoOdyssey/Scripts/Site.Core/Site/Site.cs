03ccb813f6324ceae67b5737179b428b78d0926c
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-04-16 11:31:27 +0700

    chore: add site controllers

--- Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
+++ Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
@@ -1,4 +1,5 @@
 ﻿using NekoOdyssey.Scripts.Database.Domains.Sites;
+using NekoOdyssey.Scripts.Database.Domains.Sites.Entities.SiteEntity.Repo;
 using UniRx;
 
 namespace NekoOdyssey.Scripts.Site.Core.Site
@@ -6,14 +7,20 @@ namespace NekoOdyssey.Scripts.Site.Core.Site
     public class Site
     {
         private bool _databaseInitialized;
-        
+
         public static Database.Domains.Sites.Entities.SiteEntity.Models.Site CurrentSite { get; private set; }
 
+        public bool Ready { get; private set; }
+        
+        public Subject<Unit> OnReady { get; } = new();
         public Subject<Database.Domains.Sites.Entities.SiteEntity.Models.Site> OnChangeSite { get; } = new();
 
         public void Bind()
         {
             InitializeDatabase();
+            InitializeSite();
+            Ready = true;
+            OnReady.OnNext(default);
         }
 
         public void Start()
@@ -24,15 +31,38 @@ namespace NekoOdyssey.Scripts.Site.Core.Site
         {
         }
 
-        public void InitializeDatabase()
+        private void InitializeDatabase()
         {
             if (_databaseInitialized) return;
-            using (new SitesDbContext(new() { CopyRequired = true, ReadOnly = true }));
+            using (new SitesDbContext(new() { CopyRequired = true, ReadOnly = true })) ;
             _databaseInitialized = true;
         }
 
-        public void SetSite(Database.Domains.Sites.Entities.SiteEntity.Models.Site site)
+        private void InitializeSite()
         {
+            if (CurrentSite != null) return;
+            SetSite("Prologue");
+        }
+
+        public void MoveToNextSite()
+        {
+            var currentSite = CurrentSite;
+            var nextSite = currentSite.NextSite;
+            if (nextSite == null) return;
+            var nextSiteName = nextSite.Name;
+            SetSite(nextSiteName);
+        }
+
+        public void SetSite(string siteName)
+        {
+            Database.Domains.Sites.Entities.SiteEntity.Models.Site site;
+            using (var siteDbContext = new SitesDbContext(new() { CopyRequired = false, ReadOnly = true }))
+            {
+                var siteRepo = new SiteRepo(siteDbContext);
+                site = siteRepo.FindByName(siteName);
+            }
+
+            if (site == null) return;
             CurrentSite = site;
             OnChangeSite.OnNext(site);
         }
---
+++ Assets/NekoOdyssey/Scripts/Site.Unity/Transition/SiteTransitionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace NekoOdyssey.Scripts.Site.Unity.Transition
+{
+    public class SiteTransitionController : MonoBehaviour
+    {
+        private void Start()
+        {
+            if (SiteRunner.Instance.Core.Site.Ready)
+            {
+                HandleSiteReady(default);
+                return;
+            }
+            SiteRunner.Instance.Core.Site.OnReady
+                .Subscribe(HandleSiteReady)
+                .AddTo(this);
+        }
+
+        private void HandleSiteReady(Unit _)
+        {
+            var currentSite = Core.Site.Site.CurrentSite;
+            var scenes = currentSite.Scenes;
+            foreach (var scene in scenes)
+            {
+                var sceneName = scene.Name;
+                Debug.Log($">>load_scene<< {sceneName}");
+            }
+        }
+    }
+}
