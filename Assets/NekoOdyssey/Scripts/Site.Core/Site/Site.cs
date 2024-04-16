b27293e978b11635bb97f2a24985954296746698
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-04-16 10:44:14 +0700

    chore: add site db initialization

--- Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
+++ Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
@@ -1,15 +1,19 @@
-﻿using UniRx;
+﻿using NekoOdyssey.Scripts.Database.Domains.Sites;
+using UniRx;
 
 namespace NekoOdyssey.Scripts.Site.Core.Site
 {
     public class Site
     {
+        private bool _databaseInitialized;
+        
         public static Database.Domains.Sites.Entities.SiteEntity.Models.Site CurrentSite { get; private set; }
 
         public Subject<Database.Domains.Sites.Entities.SiteEntity.Models.Site> OnChangeSite { get; } = new();
 
         public void Bind()
         {
+            InitializeDatabase();
         }
 
         public void Start()
@@ -20,11 +24,17 @@ namespace NekoOdyssey.Scripts.Site.Core.Site
         {
         }
 
+        public void InitializeDatabase()
+        {
+            if (_databaseInitialized) return;
+            using (new SitesDbContext(new() { CopyRequired = true, ReadOnly = true }));
+            _databaseInitialized = true;
+        }
+
         public void SetSite(Database.Domains.Sites.Entities.SiteEntity.Models.Site site)
         {
             CurrentSite = site;
             OnChangeSite.OnNext(site);
         }
-        
     }
 }
