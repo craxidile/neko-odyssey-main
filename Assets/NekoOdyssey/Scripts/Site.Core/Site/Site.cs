0400ad826cb9949d4c61c658a259c8eb6aa1d3d7
jirakorn <dotpng@gmail.com>
2024-04-23 17:34:59 +0700

    ii

--- Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
+++ Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
@@ -48,8 +48,8 @@ namespace NekoOdyssey.Scripts.Site.Core.Site
             //SetSite("GamePlayZone4_01", false);
             //SetSite("GamePlayZone4_02", false);
             //SetSite("GamePlayZone5_01", false);
-            //SetSite("GamePlayZone6_01", false);
-            SetSite("GamePlayZone9_01", false);
+            SetSite("GamePlayZone6_01", false);
+            //SetSite("GamePlayZone3_01", false);
         }
 
         public void MoveToNextSite()
