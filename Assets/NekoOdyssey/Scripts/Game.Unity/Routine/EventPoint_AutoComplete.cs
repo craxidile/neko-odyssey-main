802fedd77e99c60a2f528f1ed197d4fca96b6407
Linias <Linias.Aiz@gmail.com>
2024-04-26 18:21:58 +0700

    ..

---
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Routine/EventPoint_AutoComplete.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventPoint_AutoComplete : MonoBehaviour
+{
+    [SerializeField] float completeDelay = 1f;
+    public float CompleteDelay { get => completeDelay; }
+
+
+}
