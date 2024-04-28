4e67d3f349fdea82e9f3c6b413f670eddf65511e
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-04-28 17:09:36 +0700

    chore: add player cat / player quest / player site tables to game save db

---
+++ Assets/NekoOdyssey/Scripts/Database/Domains/SaveV001/PlayerCatEntity/Models/PlayerCatV001.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SpatiumInteractive.Libraries.Unity.GRU.Base;
+using SpatiumInteractive.Libraries.Unity.GRU.Contracts;
+using SQLite4Unity3d;
+
+namespace NekoOdyssey.Scripts.Database.Domains.SaveV001.PlayerCatEntity.Models
+{
+    [Serializable]
+    public class PlayerCatV001 : EntityBase<int>, IAggregateRoot
+    {
+        [NotNull] [Unique] public string CatCode { get; set; }
+        
+        [NotNull] public int DailyFeedCount { get; set; }
+        
+        [NotNull] public int DailyPlayCount { get; set; }
+        
+        [NotNull] public float Friendship { get; set; }
+    }
+}
