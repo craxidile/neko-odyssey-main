c55c6c092225b94721a2d97192d5066180a2ac98
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-06-11 19:28:49 +0700

    fix: fix skybox duplication bug

---
+++ Assets/NekoOdyssey/Scripts/Site.Unity/Transition/SiteLoaderPreparationController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace NekoOdyssey.Scripts.Site.Unity.Transition
+{
+    public class SiteLoaderPreparationController : MonoBehaviour
+    {
+        private void Start()
+        {
+            SceneManager.LoadScene("SceneLoader", LoadSceneMode.Single);
+        }
+    }
+}
