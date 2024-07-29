25a2d4c89961b62840f24eaac8229b7aac595445
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-07-29 22:58:14 +0700

    chore: add repos

---
+++ Assets/NekoOdyssey/Scripts/Database/Domains/Npc/Entities/DialogConditionCaseEntity/Repo/DialogConditionCaseRepo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NekoOdyssey.Scripts.Database.Domains.Npc.Entities.DialogConditionCaseEntity.Models;
+using SpatiumInteractive.Libraries.Unity.GRU.Contracts;
+using SpatiumInteractive.Libraries.Unity.GRU.Domain;
+using SQLite4Unity3d;
+
+namespace NekoOdyssey.Scripts.Database.Domains.Npc.Entities.DialogConditionCaseEntity.Repo
+{
+    public class DialogConditionCaseRepo : Repository<DialogConditionCase, int, SQLiteConnection>
+    {
+        public DialogConditionCaseRepo(IDbContext<SQLiteConnection> dbContext) : base(dbContext)
+        {
+        }
+
+        public ICollection<DialogConditionCase> ListByConditionId(int conditionId)
+        {
+            return _dbContext.Context.Table<DialogConditionCase>()
+                .Where(dcc => dcc.ConditionId == conditionId)
+                .ToList();
+        }
+    }
+}
---
+++ Assets/NekoOdyssey/Scripts/Database/Domains/Npc/Entities/DialogConditionOptionEntity/Repo/DialogConditionOptionRepo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NekoOdyssey.Scripts.Database.Domains.Npc.Entities.DialogConditionOptionEntity.Models;
+using SpatiumInteractive.Libraries.Unity.GRU.Contracts;
+using SpatiumInteractive.Libraries.Unity.GRU.Domain;
+using SQLite4Unity3d;
+
+namespace NekoOdyssey.Scripts.Database.Domains.Npc.Entities.DialogConditionOptionEntity.Repo
+{
+    public class DialogConditionOptionRepo : Repository<DialogConditionOption, int, SQLiteConnection>
+    {
+        public DialogConditionOptionRepo(IDbContext<SQLiteConnection> dbContext) : base(dbContext)
+        {
+        }
+
+        public ICollection<DialogConditionOption> ListByConditionId(int conditionId)
+        {
+            return _dbContext.Context.Table<DialogConditionOption>()
+                .Where(dcc => dcc.ConditionId == conditionId)
+                .ToList();
+        }
+    }
+}
