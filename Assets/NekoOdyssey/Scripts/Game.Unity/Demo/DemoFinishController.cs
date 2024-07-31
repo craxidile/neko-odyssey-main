56621ce50955ad08ec0899a4f917be57cdcc08a9
Linias <Linias.Aiz@gmail.com>
2024-07-31 18:54:39 +0700

    fix demo video

--- Assets/NekoOdyssey/Scripts/Game.Unity/Demo/DemoFinishController.cs
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Demo/DemoFinishController.cs
@@ -2,28 +2,45 @@
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UniRx;
 
 namespace NekoOdyssey.Scripts.Game.Unity.Demo
 {
     public class DemoFinishController : MonoBehaviour
     {
-        public CanvasGroup fading;
+        //public CanvasGroup fading;
 
         private void Start()
         {
-            fading.GetComponent<CanvasGroup>().alpha = 0;
-            DOVirtual.DelayedCall(10f, () => { fading.DOFade(1f, 2f); });
-            DOVirtual.DelayedCall(12f, () =>
+            //fading.GetComponent<CanvasGroup>().alpha = 0;
+            //DOVirtual.DelayedCall(10f, () => { fading.DOFade(1f, 2f); });
+            //DOVirtual.DelayedCall(15f, () =>
+            //{
+            //    Debug.Log("load DemoFinishVideo 0 ");
+            //    if (GameRunner.Instance.CsvHolder.playFinishDemoVideo)
+            //    {
+            //        Debug.Log("load DemoFinishVideo 1 ");
+            //        SceneManager.LoadScene("DemoFinishVideo");
+            //    }
+            //    else
+            //    {
+            //        SiteRunner.Instance.Core.Site.SetSite("FinishDemo");
+            //    }
+            //});
+
+
+            if (GameRunner.Instance.CsvHolder.playFinishDemoVideo)
             {
-                if (GameRunner.Instance.CsvHolder.playFinishDemoVideo)
+                Debug.Log("load DemoFinishVideo 1");
+
+                SiteRunner.Instance.Core.Site.OnChangeSite.Subscribe(_ =>
                 {
-                    SceneManager.LoadSceneAsync("SceneLoader");
-                }
-                else
-                {
-                    SiteRunner.Instance.Core.Site.SetSite("FinishDemo");
-                }
-            });
+                    Debug.Log("load DemoFinishVideo 2");
+
+                    SceneManager.LoadScene("DemoFinishVideo");
+                }).AddTo(this);
+            }
         }
+
     }
 }
