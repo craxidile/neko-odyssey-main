46b0590e6eb74d029ecb3f05178f4c7e271e6554
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-08-18 19:33:51 +0700

    chore: add daily summary canvas

---
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Uis/DailySummaryCanvas/DailySummaryController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace NekoOdyssey.Scripts.Game.Unity.Uis.DailySummaryCanvas
+{
+    public class DailySummaryController : MonoBehaviour
+    {
+        public Text likeCountText;
+        public Text followerCountText;
+        public Text commentCountText;
+        public Text catInteractionText;
+        public Text photoCountText;
+        public Text moneyText;
+        public ButtonHover okButton;
+    }
+}
