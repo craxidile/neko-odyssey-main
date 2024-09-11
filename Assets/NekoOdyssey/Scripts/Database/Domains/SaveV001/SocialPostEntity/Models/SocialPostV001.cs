bc63ce95d04d7ba03aba24a5e6df62a400fa0660
jirakorn <dotpng@gmail.com>
2024-09-11 13:22:25 +0700

    fix: fix ui static bug in game main scene

--- Assets/NekoOdyssey/Scripts/Database/Domains/SaveV001/SocialPostEntity/Models/SocialPostV001.cs
+++ Assets/NekoOdyssey/Scripts/Database/Domains/SaveV001/SocialPostEntity/Models/SocialPostV001.cs
@@ -5,6 +5,7 @@ using NekoOdyssey.Scripts.Database.Domains.SaveV001.CatPhotoEntity.Models;
 using SpatiumInteractive.Libraries.Unity.GRU.Base;
 using SpatiumInteractive.Libraries.Unity.GRU.Contracts;
 using SQLite4Unity3d;
+using UnityEngine;
 
 namespace NekoOdyssey.Scripts.Database.Domains.SaveV001.SocialPostEntity.Models
 {
