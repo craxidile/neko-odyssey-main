13bd697c1433becd037ba097e10e705691cc1dbc
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-10-02 23:56:23 +0700

    chore: add sound to birds while flapping

---
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Audios/BirdAnimator/BirdAnimatorStateBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace NekoOdyssey.Scripts.Game.Unity.Audios.BirdAnimator
+{
+    public class BirdAnimatorStateBehaviour : StateMachineBehaviour
+    {
+        private readonly int _eatState = Animator.StringToHash("Fly");
+
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.shortNameHash == _eatState)
+            {
+                GameRunner.Instance.Core.Audios.AudioToClone.OnNext(("SFX_Bird_Flap", 3f));
+            }
+        }
+    }
+}
