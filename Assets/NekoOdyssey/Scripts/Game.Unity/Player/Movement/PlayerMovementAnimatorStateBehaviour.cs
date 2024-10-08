5037678045ae03e91e8b29585ea84e212fc32126
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-10-08 12:03:17 +0700

    fix: fix head shake bugs

---
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Player/Movement/PlayerMovementAnimatorStateBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using NekoOdyssey.Scripts.Game.Core.Cat;
+using UnityEngine;
+
+namespace NekoOdyssey.Scripts.Game.Unity.Player.Feed
+{
+    public class PlayerMovementAnimatorStateBehaviour : StateMachineBehaviour
+    {
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            if (stateInfo.shortNameHash != Animator.StringToHash($"ShakeHead")) return;
+            GameRunner.Instance.Core.Player.FinishHeadShake();
+        }
+    }
+}
