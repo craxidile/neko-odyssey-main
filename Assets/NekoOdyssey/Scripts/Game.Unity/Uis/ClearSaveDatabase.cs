8fdd897fd449716afdf05e7d5710ef17f5a80bf1
Chanan Gosonsaksakun <craxidile@gmail.com>
2024-10-12 20:33:12 +0700

    fix: fix dialog not-displayed bug

--- Assets/NekoOdyssey/Scripts/Game.Unity/Uis/ClearSaveDatabase.cs
+++ Assets/NekoOdyssey/Scripts/Game.Unity/Uis/ClearSaveDatabase.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using NekoOdyssey.Scripts.Database.Domains;
 using NekoOdyssey.Scripts.Database.Domains.SaveV001;
+using NekoOdyssey.Scripts.Database.Domains.SaveV001.GameSettingsEntity.Models;
+using NekoOdyssey.Scripts.Database.Domains.SaveV001.GameSettingsEntity.Repo;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,7 +12,22 @@ namespace NekoOdyssey.Scripts.Game.Unity.Uis
     {
         private void Start()
         {
+            GameSettingsV001 settings;
+            using (var context =
+                   new SaveV001DbContext(new() { CopyMode = DbCopyMode.CopyIfNotExists, ReadOnly = true }))
+            {
+                var settingsRepo = new GameSettingsV001Repo(context);
+                settings = settingsRepo.Load();
+            }
+
             using (new SaveV001DbContext(new() { CopyMode = DbCopyMode.ForceCopy, ReadOnly = false })) ;
+
+            using (var context =
+                   new SaveV001DbContext(new() { CopyMode = DbCopyMode.CopyIfNotExists, ReadOnly = false }))
+            {
+                var settingsRepo = new GameSettingsV001Repo(context);
+                settingsRepo.Update(settings);
+            }
         }
     }
 }
