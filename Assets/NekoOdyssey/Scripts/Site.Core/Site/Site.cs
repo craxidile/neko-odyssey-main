43ae73658d9a8c487bc0e2d2544e83766de7a060
jirakorn <dotpng@gmail.com>
2024-10-14 07:18:39 +0700

    ii

--- Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
+++ Assets/NekoOdyssey/Scripts/Site.Core/Site/Site.cs
@@ -56,7 +56,7 @@ namespace NekoOdyssey.Scripts.Site.Core.Site
             // SetSite("GamePlayZone6_02", false);
             // SetSite("GamePlayZone3_01", false);
             // SetSite("GamePlayZone3_02", false);
-            // SetSite("P0S04", false);
+            // SetSite("P0S02", false);
             // SetSite("NekoInside28BedroomFinal", false);
             SetSite("StartTitle", false);
             // SetSite("MiniGameFishing", false);
